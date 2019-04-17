bd3e950a6355b544f19c39a875046a28329b7a3b
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-04-17 23:24:18 +0200

    login component in app

--- client/chrome-extension/src/app/hooks-contexts/useSignUpOrSignIn.ts
+++ client/chrome-extension/src/app/hooks-contexts/useSignUpOrSignIn.ts
@@ -5,10 +5,10 @@ import { useHttp } from './useHttp'
 
 const config = {
   Auth: {
-    identityPoolId: process.env.COGNITO_IDENTITY_POOL_ID,
-    region: process.env.COGNITO_REGION,
-    userPoolId: process.env.COGNITO_USER_POOL_ID,
-    userPoolWebClientId: process.env.COGNITO_USER_POOL_WEB_CLIENT_ID
+    identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+    region: process.env.REACT_APP_COGNITO_REGION,
+    userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_COGNITO_USER_POOL_WEB_CLIENT_ID
   }
 }
 
--- client/pwa/src/App.tsx
+++ client/pwa/src/App.tsx
@@ -1,48 +1,101 @@
-import React from 'react'
-import { showName, add } from 'shared'
+import React, { FC, useContext, useEffect } from 'react'
+import { showName, add } from '@bookmarks/shared'
 // import { Typography } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import ListItemText from '@material-ui/core/ListItemText'
-import Something from 'shared/components/Something'
-// import Another from 'shared/Another'
+import Something from '@bookmarks/shared/components/Something'
+import {
+  UserContextProvider,
+  UserContext
+} from '@bookmarks/extension/src/app/hooks-contexts/useUser'
+import {
+  BrowserRouter,
+  Route,
+  RouteComponentProps,
+  Redirect,
+  RouteProps,
+  Link
+} from 'react-router-dom'
 
-// import ListItemText from '@material-ui/core/ListItemText'
+import LoginOrSignUp from '@bookmarks/extension/src/app/view/Login'
 
-const App: React.FC = () => {
-  console.log(showName())
+const AuthRoute: FC<any> = props => {
+  const { component: Component, ...restProps } = props
+  const { user, fetching } = useContext(UserContext)
+
+  console.log(fetching, !!user)
+
+  if (fetching) return null
+
   return (
-    <Typography variant="h2">
-      <Something />
-      {/* <Another /> */}
-      App! {add(1, 2)} {showName()}
-    </Typography>
+    <Route
+      {...restProps}
+      render={routeProps =>
+        user ? <Component {...routeProps} /> : <Redirect to="/login" />
+      }
+    />
   )
-  // return (
-  //   <Typography variant="h2">
-  //     App! {add(1, 2)} {showName()}
-  //   </Typography>
-  // )
-  // const { fetching, user } = useContext(UserContext)
-  // const { groups } = useContext(GroupContext)
-  // return (
-  // <ContextProviders>
-  //   <div>
-  //     {!fetching &&
-  //       user &&
-  //       groups &&
-  //       groups.map((group, i) => (
-  //         <Bookmarks
-  //           bookmarks={group.bookmarks}
-  //           groupId={group._id}
-  //           groupIndex={i}
-  //         />
-  //       ))}
-  //     {!user && !fetching && <LoginOrSignUp />}
-  //     {fetching && <div>Loading</div>}
-  //   </div>
-  // </ContextProviders>
-  // )
 }
+
+const Routes = () => {
+  return (
+    <>
+      <Route exact path="/" component={() => <div>Top!</div>} />
+      <Route
+        exact
+        path="/login"
+        component={(props: RouteComponentProps) => (
+          <LoginOrSignUp
+            onLogin={() => {
+              props.history.push('/bookmarks')
+            }}
+          />
+        )}
+      />
+      <AuthRoute
+        exact
+        path="/bookmarks"
+        component={() => <div>Bookmarks</div>}
+      />
+    </>
+  )
+}
+const App: FC<{}> = () => {
+  return (
+    <BrowserRouter>
+      <UserContextProvider>
+        <Routes />
+        <Link to="/bookmarks">bookmarks</Link>
+      </UserContextProvider>
+    </BrowserRouter>
+  )
+}
+
+// return (
+//   <Typography variant="h2">
+//     App! {add(1, 2)} {showName()}
+//   </Typography>
+// )
+// const { fetching, user } = useContext(UserContext)
+// const { groups } = useContext(GroupContext)
+// return (
+// <ContextProviders>
+//   <div>
+//     {!fetching &&
+//       user &&
+//       groups &&
+//       groups.map((group, i) => (
+//         <Bookmarks
+//           bookmarks={group.bookmarks}
+//           groupId={group._id}
+//           groupIndex={i}
+//         />
+//       ))}
+//     {!user && !fetching && <LoginOrSignUp />}
+//     {fetching && <div>Loading</div>}
+//   </div>
+// </ContextProviders>
+// )
 
 // const ContextProviders: FC<{}> = ({ children }) => {
 //   return (
