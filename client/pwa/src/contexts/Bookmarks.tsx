7a55dad67b3ccb35fa6622c86df984de3ee8d284
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-05-12 13:40:21 +0200

    update pwa

--- client/pwa/src/contexts/Bookmarks.tsx
+++ client/pwa/src/contexts/Bookmarks.tsx
@@ -1,34 +1,23 @@
-import { Omit } from '@material-ui/core'
-import axios from 'axios'
-import React, { createContext, FC } from 'react'
-import { API_BOOKMARK_URL } from '@bookmarks/extension/src/constants'
-import { CreateBookmarkInput } from '@bookmarks/extension/src/app/api/bookmarks'
+import {
+  bookmarksAPI,
+  CreateBookmarkInput
+} from '@bookmarks/extension/src/app/api/bookmarks'
 import { IBookmark, IGroup } from '@bookmarks/extension/src/app/types'
-
-interface CreateBookmarkResponse {
-  bookmark: IBookmark
-}
+import { Omit } from '@material-ui/core'
+import React, { createContext, FC } from 'react'
 
 export type UpdateBookmarkInput = Omit<Partial<IBookmark>, 'tags'> & {
   tags?: string
 }
 
 export const useBookmarks = () => {
+  //
   const createBookmark = async (
     groupId: IGroup['_id'],
     input: CreateBookmarkInput
   ) => {
-    const categories = input.tags ? input.tags.join(',') : ''
-
-    const { data } = await axios.post<CreateBookmarkResponse>(
-      API_BOOKMARK_URL,
-      {
-        ...input,
-        group: groupId,
-        categories
-      }
-    )
-    return data.bookmark
+    const bookmark = await bookmarksAPI.create(groupId, input)
+    return bookmark
   }
 
   return {
